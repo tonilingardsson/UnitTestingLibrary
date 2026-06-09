46feb62eef9472c726e4c7fedb572c5916b9612b
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-09 19:07:37 +0200

    Created the first test for the library system

--- UnitTestingLibrary.Tests/Test1.cs
+++ UnitTestingLibrary.Tests/Test1.cs
@@ -1,11 +1,24 @@
-﻿namespace UnitTestingLibrary.Tests
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestingLibrary;
+
+namespace UnitTestingLibrary.Tests
 {
     [TestClass]
-    public sealed class Test1
+    public class LibrarySystemTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddBook_ShouldAddBook_WhenISBNIsUnique()
         {
+            // Arrange
+            var library = new LibrarySystem();
+            var book = new Book("Clean Code", "Robert C. Martin", "1111111111", 2008);
+
+            // Act
+            var result = library.AddBook(book);
+
+            // Assert 
+            Assert.IsTrue(result);
+            Assert.IsNotNull(library.SearchByISBN("1111111111"));
         }
     }
 }
